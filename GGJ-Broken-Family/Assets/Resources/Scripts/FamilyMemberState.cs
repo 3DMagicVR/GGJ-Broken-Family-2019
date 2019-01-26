4c8c383631d93b68cfcd210b31e980cad12f94e2
3D Magic VR <34051446+3DMagicVR@users.noreply.github.com>
2019-01-26 12:57:23 -0600

    Contenido adicional
    Animaciones de sprties.

---
+++ GGJ-Broken-Family/Assets/Resources/Scripts/FamilyMemberState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FamilyMemberState : MonoBehaviour {
+    public enum FamilyMember {
+        Boy,
+        Girl,
+        Son,
+        Doughter,
+        Mother,
+        Father,
+        None
+    }
+
+    FamilyMember member = FamilyMember.None;
+
+    private void SetViewRange () {
+        
+    }
+}
---
+++ GGJ-Broken-Family/Assets/Resources/Scripts/SpotTracker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SpotTracker : MonoBehaviour
+{
+    [SerializeField]private Camera cameraTracker;
+
+    void Update()
+    {
+        Vector3 trackerPosition = cameraTracker.transform.position;
+        trackerPosition.z = 0;
+        transform.position = trackerPosition;
+    }
+}
